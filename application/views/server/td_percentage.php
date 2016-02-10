00a6ab648449952193218cd5a6cde8a42c729fd0
phaniso <9phaniso9@gmail.com>
2016-02-10 13:43:41 +0100

    delete unused views

--- application/views/server/td_percentage.php
+++
@@ -1,5 +0,0 @@
-<div class="progress">
-                <div class="<?php echo getProgressBar($percents); ?>" role="progressbar" aria-valuenow="<?php echo $percents; ?>" aria-valuemin="0" aria-valuemax="100" style="min-width: 20px; width: <?php echo $percents; ?>%;">
-                <?php echo $percents; ?>%
-                </div>
-            </div>
